f9f35889d28d75852affe73a79d46eb48249d1e7
zhangjinshuai <zhangjinshuai@njfae.cn>
2018-10-09 15:31:59 +0800

    增加邮件测试

---
+++ app/Console/Commands/SendMail.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Mail\activeUser;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+
+class SendMail extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sendMail';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Test to send mail';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        try {
+            Mail::to("eachcan@163.com")->send(new activeUser("网站名", "http://myvpn.tk/url"));
+        } catch (\Exception $e) {
+            echo $e->getTraceAsString();
+        }
+    }
+}
